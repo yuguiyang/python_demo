ad164d56d9edfe7f82378f998d73c3d0711aab07
yuguiyang <773808318@qq.com>
2017-08-23 00:24:38 +0800

    修改豆瓣影评

--- code/crawler/bs_douban_movie_zhanlang.py
+++ code/crawler/bs_douban_movie_zhanlang.py
@@ -8,24 +8,30 @@ Created on Tue Aug 22 16:13:24 2017
 import os
 import urllib
 from bs4 import BeautifulSoup
+import jieba
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
 file_name = 'douban_movie_zhanlang.txt'
 
+#根据URL，获取页面源码
 def getHtml(url):
     req = urllib.request.Request(url)
     page = urllib.request.urlopen(req).read().decode('utf-8')
     
     return page
 
-def clearFile(file_name):
-    if os.path.exists(file_name):
-        os.remove(file_name)
+#删除文件
+def clearFile(targetFile):
+    if os.path.exists(targetFile):
+        os.remove(targetFile)
     
-
-def save2file(lines,file_name):
-    with open(file_name,'a') as file:
+#将数据保存到文件
+def save2file(lines,targetFile):
+    with open(targetFile,'a') as file:
         file.writelines(lines)
-        
+
+#解析当前页面        
 def parseCurrentPage(html):
     soup = BeautifulSoup(html, "html.parser")
     
@@ -36,24 +42,47 @@ def parseCurrentPage(html):
     for com in p_comments:
         p_lines.append(com.contents[0].strip()+'\n')
     
-    save2file(p_lines,file_name)
-    
-    #获取下一页信息
+    #获取下一页信息，可以通过这个获取数据
     p_next = soup.select_one('div#paginator a.next')
-    print(p_next)
+    #print(p_next)
     print(p_next['href'])
     
+    return p_lines
+    
+def showWordCloud(targetFile):
+    #指定字体，是为了显示中文
+    font = r'C:\Windows\Fonts\simsun.ttc'
+    with open(targetFile) as file:
+        comments = file.read()
+        text_cut = jieba.cut(comments , cut_all=False)
+        
+        #指定需要提出的词语
+        stopwords = {u'个',u'一个',u'这个',u'个人',u'不是',u'就是',u'一部',u'这部'
+                     ,u'我们',u'所以',u'不会',u'这种',u'没有',u'各种',u'觉得'
+                     ,u'真的',u'知道',u'还是',u'但是',u'可以',u'这么',u'因为',u'很多'}
+        print('stopwords',stopwords)
+        
 
+        wordcloud = WordCloud(font_path=font,width=800,height=400,
+                              background_color='white',stopwords=stopwords
+                              ).generate(' '.join(text_cut))
+        
+        plt.imshow(wordcloud, interpolation='bilinear')
+        plt.axis("off")
+        
 def main():   
-    global file_name
+    url = 'https://movie.douban.com/subject/26363254/comments?start={0}&limit=20&sort=new_score&status=P'
+    
     clearFile(file_name)
+    #直接遍历200条评论，这里要注意，超过多少页后，需要登录才可以，这里暂时还没有做，就到200
+    for i in range(0,200,20):    
+        print(url.format(i))
+        html = getHtml(url.format(i))
+        movie_comments = parseCurrentPage(html)
+        save2file(movie_comments,file_name)
     
-    url = 'https://movie.douban.com/subject/26363254/comments?start=0&limit=20&sort=new_score&status=P'
-
-    html = getHtml(url)
-    
-    parseCurrentPage(html)
-    
+    #显示词云图
+    showWordCloud(file_name)
 
 if __name__=='__main__':
     main()
--- code/matplotlib/plt_flash_demo_1.py
+++ code/matplotlib/plt_flash_demo_1.py
@@ -8,23 +8,33 @@ Created on Thu Aug 17 18:19:08 2017
 import numpy as np  
 import matplotlib.pyplot as plt  
 import matplotlib.animation as animation  
+from datetime import datetime 
   
 
 fig,axes = plt.subplots()  
 axes.plot(np.random.rand(10))  
 
+t_count = 0
+
 #重新绘制图形
 def update_line(data): 
+    global t_count
+    t_count = t_count+1
+    
+    print('\n')
+    print(t_count,':',datetime.now(),':',data)
+    print('------------------------------------\n')
     #清空当前轴
     plt.cla()
     #重新绘图
-    axes.plot(data)
+    axes.plot(np.random.rand(10))
 
 #生成绘制图形的数据
 def create_line_data():
     yield np.random.rand(10)  
 
 #传入的fig中，调用update_line函数，将create_line_data作为参数传给update_line，5秒调用一次
-ani = animation.FuncAnimation(fig, update_line, create_line_data, interval=5*1000)  
+#ani = animation.FuncAnimation(fig, update_line, 5, interval=2*1000)  
+ani = animation.FuncAnimation(fig, update_line, 5, interval=5000,bl)  
 
 plt.show() 
